9fdb6109f865d0a55162f68ee97a4aa6e297a50e
Kyle Downey <kyle.downey@gmail.com>
2020-04-13 09:17:33 -0400

    fixed config; added back BeakerX

--- jupyterlab/jupyter_notebook_config.py
+++ jupyterlab/jupyter_notebook_config.py
@@ -4,4 +4,4 @@ c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
 c.NotebookApp.token = ''
 c.NotebookApp.password = ''
-c.NotebookApp.notebook_dir = "/home/serenity/dev/shadows/serenity"
+c.NotebookApp.notebook_dir = "/home/serenity/dev/shadows"
