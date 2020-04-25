e635fafa371f5ac3c4a6b81fe256ac0d1236d63c
Kyle Downey <kyle.downey@gmail.com>
2020-04-24 20:36:49 -0400

    Pull up common feed handler logic as base classes
    AB#29

--- src/serenity/fh/feedhandler.py
+++ src/serenity/fh/feedhandler.py
@@ -1,11 +1,19 @@
+import asyncio
 import logging
 from abc import ABC, abstractmethod
+from datetime import datetime
 from enum import Enum
+from pathlib import Path
 from typing import List
 
-from tau.core import Signal
+from tau.core import Signal, MutableSignal, NetworkScheduler, Event
+from tau.event import Do
 
+from serenity.db import TypeCodeCache, InstrumentCache, connect_serenity_db
 from serenity.model.exchange import ExchangeInstrument
+from serenity.model.order import Side
+from serenity.tickstore.journal import Journal
+from serenity.utils import init_logging, custom_asyncio_error_handler
 
 
 class FeedState(Enum):
@@ -59,13 +67,50 @@ class Feed(ABC):
         pass
 
 
+class WebsocketFeed(Feed):
+    def __init__(self, scheduler: NetworkScheduler, type_code_cache: TypeCodeCache,
+                 ws_uri: str, exch_instrument: ExchangeInstrument):
+        self.scheduler = scheduler
+        self.type_code_cache = type_code_cache
+        self.ws_uri = ws_uri
+        self.instrument = exch_instrument
+
+        self.buy_code = type_code_cache.get_by_code(Side, 'Buy')
+        self.sell_code = type_code_cache.get_by_code(Side, 'Sell')
+
+        self.trades = None
+        self.quotes = None
+
+        self.feed_state = MutableSignal(FeedState.INIT)
+
+    def get_instrument(self) -> ExchangeInstrument:
+        return self.instrument
+
+    def get_trades(self) -> Signal:
+        return self.trades
+
+    def get_quotes(self) -> Signal:
+        return self.quotes
+
+    def get_feed_state(self) -> Signal:
+        return self.feed_state
+
+    async def start(self):
+        self.scheduler.schedule_update(self.feed_state, FeedState.STARTING)
+        await self._subscribe_trades_and_quotes()
+
+    @abstractmethod
+    async def _subscribe_trades_and_quotes(self):
+        pass
+
+
 class FeedHandler(ABC):
     """
     A connector for exchange marketdata.
     """
 
-    @abstractmethod
-    def get_uri_scheme(self) -> str:
+    @staticmethod
+    def get_uri_scheme() -> str:
         """
         Gets the short string name like 'phemex' or 'kraken' for this feedhandler.
         """
@@ -92,6 +137,48 @@ class FeedHandler(ABC):
         phemex:prod:BTCUSD or coinbase:test:BTC-USD. Raises an exception if the scheme:instance
         portion does not match this FeedHandler.
         """
+        pass
+
+
+class WebsocketFeedHandler(FeedHandler):
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, scheduler: NetworkScheduler, instrument_cache: InstrumentCache,
+                 instance_id: str):
+        self.scheduler = scheduler
+        self.instrument_cache = instrument_cache
+        self.instance_id = instance_id
+
+        self.instruments = []
+        self.known_instrument_ids = {}
+        self.price_scaling = {}
+        self._load_instruments()
+
+    def get_instance_id(self) -> str:
+        return self.instance_id
+
+    def get_instruments(self) -> List[ExchangeInstrument]:
+        return self.instruments
+
+    def get_feed(self, uri: str) -> Feed:
+        (scheme, instance_id, instrument_id) = uri.split(':')
+        if scheme != self.get_uri_scheme():
+            raise ValueError(f'Unsupported URI scheme: {scheme}')
+        if instance_id != self.get_instance_id():
+            raise ValueError(f'Unsupported instance ID: {instance_id}')
+        if instrument_id not in self.known_instrument_ids:
+            raise ValueError(f'Unknown exchange Instrument: {instrument_id}')
+        self.logger.info(f'Acquiring Feed for {uri}')
+
+        return self._create_feed(self.scheduler, self.instrument_cache.get_type_code_cache(),
+                                 self.known_instrument_ids[instrument_id])
+
+    @abstractmethod
+    def _create_feed(self, scheduler, type_code_cache: TypeCodeCache, instrument):
+        pass
+
+    @abstractmethod
+    def _load_instruments(self):
         pass
 
 
@@ -128,3 +215,58 @@ class FeedHandlerRegistry:
     @staticmethod
     def _get_fh_key(feedhandler: FeedHandler) -> str:
         return f'{feedhandler.get_uri_scheme()}:{feedhandler.get_instance_id()}'
+
+
+def ws_fh_main(create_fh, uri_scheme: str, instance_id: str, journal_path: str, db: str):
+    init_logging()
+    logger = logging.getLogger(__name__)
+
+    conn = connect_serenity_db()
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    instr_cache = InstrumentCache(cur, TypeCodeCache(cur))
+
+    scheduler = NetworkScheduler()
+    registry = FeedHandlerRegistry()
+    fh = create_fh(scheduler, instr_cache, instance_id)
+    registry.register(fh)
+
+    for instrument in fh.get_instruments():
+        feed = registry.get_feed(f'{uri_scheme}:{instance_id}:{instrument.get_exchange_instrument_code()}')
+
+        # subscribe to FeedState in advance so we know when the Feed is ready to subscribe trades
+        class SubscribeTrades(Event):
+            def __init__(self, trade_feed: Feed):
+                self.trade_feed = trade_feed
+                instrument_code = trade_feed.get_instrument().get_exchange_instrument_code()
+                self.journal = Journal(Path(f'{journal_path}/{db}/{instrument_code}'))
+                self.appender = self.journal.create_appender()
+
+            def on_activate(self) -> bool:
+                if self.trade_feed.get_feed_state().get_value() == FeedState.LIVE:
+                    trades = self.trade_feed.get_trades()
+                    Do(scheduler.get_network(), trades, lambda: self.on_subscribe(trades.get_value()))
+                return False
+
+            def on_subscribe(self, trade):
+                logger.info(trade)
+
+                self.appender.write_double(datetime.utcnow().timestamp())
+                self.appender.write_long(trade.get_trade_id())
+                self.appender.write_long(trade.get_trade_id())
+                self.appender.write_string(trade.get_instrument().get_exchange_instrument_code())
+                self.appender.write_short(1 if trade.get_side().get_type_code() == 'Buy' else 0)
+                self.appender.write_double(trade.get_qty())
+                self.appender.write_double(trade.get_price())
+
+        scheduler.get_network().connect(feed.get_feed_state(), SubscribeTrades(feed))
+
+        # async start the feed
+        asyncio.ensure_future(feed.start())
+
+    # crash out on any exception
+    asyncio.get_event_loop().set_exception_handler(custom_asyncio_error_handler)
+
+    # go!
+    asyncio.get_event_loop().run_forever()
--- src/serenity/fh/phemex_fh.py
+++ src/serenity/fh/phemex_fh.py
@@ -1,60 +1,26 @@
-import asyncio
 import json
 import logging
-from datetime import datetime
-from pathlib import Path
 from typing import List
 
 import fire
 import websockets
 from phemex import PhemexConnection
-from tau.core import Signal, MutableSignal, NetworkScheduler, Event
-from tau.event import Do
+from tau.core import MutableSignal, NetworkScheduler
 from tau.signal import Map, Filter
 
-from serenity.db import connect_serenity_db, TypeCodeCache, InstrumentCache
-from serenity.fh.feedhandler import FeedHandler, Feed, FeedHandlerRegistry, FeedState
+from serenity.db import TypeCodeCache, InstrumentCache
+from serenity.fh.feedhandler import FeedState, WebsocketFeedHandler, WebsocketFeed, \
+    ws_fh_main
 from serenity.model.exchange import ExchangeInstrument
 from serenity.model.marketdata import Trade
-from serenity.model.order import Side
-from serenity.tickstore.journal import Journal
-from serenity.utils import init_logging, FlatMap, custom_asyncio_error_handler
+from serenity.utils import FlatMap
 
 
-class PhemexFeed(Feed):
-    logger = logging.getLogger(__name__)
-
-    def __init__(self, scheduler: NetworkScheduler, type_code_cache: TypeCodeCache,
-                 ws_uri: str, exch_instrument: ExchangeInstrument, price_scale: int):
-        self.scheduler = scheduler
-        self.type_code_cache = type_code_cache
-        self.ws_uri = ws_uri
-        self.instrument = exch_instrument
+class PhemexFeed(WebsocketFeed):
+    def __init__(self, scheduler: NetworkScheduler, type_code_cache: TypeCodeCache, ws_uri: str,
+                 exch_instrument: ExchangeInstrument, price_scale: int):
+        super().__init__(scheduler, type_code_cache, ws_uri, exch_instrument)
         self.price_scale = price_scale
-
-        self.buy_code = type_code_cache.get_by_code(Side, 'Buy')
-        self.sell_code = type_code_cache.get_by_code(Side, 'Sell')
-
-        self.trades = None
-        self.quotes = None
-
-        self.feed_state = MutableSignal(FeedState.INIT)
-
-    def get_instrument(self) -> ExchangeInstrument:
-        return self.instrument
-
-    def get_trades(self) -> Signal:
-        return self.trades
-
-    def get_quotes(self) -> Signal:
-        raise NotImplementedError()
-
-    def get_feed_state(self) -> Signal:
-        return self.feed_state
-
-    async def start(self):
-        self.scheduler.schedule_update(self.feed_state, FeedState.STARTING)
-        await self._subscribe_trades_and_quotes()
 
     async def _subscribe_trades_and_quotes(self):
         subscribe_msg = {
@@ -95,14 +61,10 @@ class PhemexFeed(Feed):
         return trade_list
 
 
-class PhemexFeedHandler(FeedHandler):
+class PhemexFeedHandler(WebsocketFeedHandler):
     logger = logging.getLogger(__name__)
 
-    def __init__(self, scheduler: NetworkScheduler, instrument_cache: InstrumentCache,
-                 instance_id: str = 'prod'):
-        self.scheduler = scheduler
-        self.instrument_cache = instrument_cache
-        self.instance_id = instance_id
+    def __init__(self, scheduler: NetworkScheduler, instrument_cache: InstrumentCache, instance_id: str = 'prod'):
         if instance_id == 'prod':
             self.ws_uri = 'wss://phemex.com/ws'
             self.phemex = PhemexConnection()
@@ -112,32 +74,16 @@ class PhemexFeedHandler(FeedHandler):
         else:
             raise ValueError(f'Unknown instance_id: {instance_id}')
 
-        self.instruments = []
-        self.known_instrument_ids = {}
-        self.price_scaling = {}
-        self._load_instruments()
+        # ensure we've initialized PhemexConnection before loading instruments in super()
+        super().__init__(scheduler, instrument_cache, instance_id)
 
-    def get_uri_scheme(self) -> str:
+    @staticmethod
+    def get_uri_scheme() -> str:
         return 'phemex'
 
-    def get_instance_id(self) -> str:
-        return self.instance_id
-
-    def get_instruments(self) -> List[ExchangeInstrument]:
-        return self.instruments
-
-    def get_feed(self, uri: str) -> Feed:
-        (scheme, instance_id, instrument_id) = uri.split(':')
-        if scheme != self.get_uri_scheme():
-            raise ValueError(f'Unsupported URI scheme: {scheme}')
-        if instance_id != self.get_instance_id():
-            raise ValueError(f'Unsupported instance ID: {instance_id}')
-        if instrument_id not in self.known_instrument_ids:
-            raise ValueError(f'Unknown exchange Instrument: {instrument_id}')
-        self.logger.info(f'Acquiring Feed for {uri}')
-
-        return PhemexFeed(self.scheduler, self.instrument_cache.get_type_code_cache(), self.ws_uri,
-                          self.known_instrument_ids[instrument_id], self.price_scaling[instrument_id])
+    def _create_feed(self, scheduler, type_code_cache: TypeCodeCache, instrument: ExchangeInstrument):
+        price_scale = self.price_scaling[instrument.get_exchange_instrument_code()]
+        return PhemexFeed(scheduler, type_code_cache, self.ws_uri, instrument, price_scale)
 
     def _load_instruments(self):
         self.logger.info("Downloading supported products")
@@ -157,59 +103,12 @@ class PhemexFeedHandler(FeedHandler):
             self.price_scaling[symbol] = price_scale
 
 
+def create_fh(scheduler: NetworkScheduler, instrument_cache: InstrumentCache, instance_id):
+    return PhemexFeedHandler(scheduler, instrument_cache, instance_id)
+
+
 def main(instance_id: str = 'prod', journal_path: str = '/behemoth/journals/'):
-    init_logging()
-    logger = logging.getLogger(__name__)
-
-    conn = connect_serenity_db()
-    conn.autocommit = True
-    cur = conn.cursor()
-
-    instr_cache = InstrumentCache(cur, TypeCodeCache(cur))
-
-    scheduler = NetworkScheduler()
-    registry = FeedHandlerRegistry()
-    fh = PhemexFeedHandler(scheduler, instr_cache)
-    registry.register(fh)
-
-    for instrument in fh.get_instruments():
-        feed = registry.get_feed(f'phemex:{instance_id}:{instrument.get_exchange_instrument_code()}')
-
-        # subscribe to FeedState in advance so we know when the Feed is ready to subscribe trades
-        class SubscribeTrades(Event):
-            def __init__(self, trade_feed: Feed):
-                self.trade_feed = trade_feed
-                instrument_code = trade_feed.get_instrument().get_exchange_instrument_code()
-                self.journal = Journal(Path(f'{journal_path}/PHEMEX_TRADES/{instrument_code}'))
-                self.appender = self.journal.create_appender()
-
-            def on_activate(self) -> bool:
-                if self.trade_feed.get_feed_state().get_value() == FeedState.LIVE:
-                    trades = self.trade_feed.get_trades()
-                    Do(scheduler.get_network(), trades, lambda: self.on_subscribe(trades.get_value()))
-                return False
-
-            def on_subscribe(self, trade):
-                logger.info(trade)
-
-                self.appender.write_double(datetime.utcnow().timestamp())
-                self.appender.write_long(trade.get_trade_id())
-                self.appender.write_long(trade.get_trade_id())
-                self.appender.write_string(trade.get_instrument().get_exchange_instrument_code())
-                self.appender.write_short(1 if trade.get_side().get_type_code() == 'Buy' else 0)
-                self.appender.write_double(trade.get_qty())
-                self.appender.write_double(trade.get_price())
-
-        scheduler.get_network().connect(feed.get_feed_state(), SubscribeTrades(feed))
-
-        # async start the feed
-        asyncio.ensure_future(feed.start())
-
-    # crash out on any exception
-    asyncio.get_event_loop().set_exception_handler(custom_asyncio_error_handler)
-
-    # go!
-    asyncio.get_event_loop().run_forever()
+    ws_fh_main(create_fh, 'phemex', instance_id, journal_path, 'PHEMEX_TRADES')
 
 
 if __name__ == '__main__':
