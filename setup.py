f229cb576fa8cba37ffc50f93d0e304149849400
Kyle Downey <kyle.downey@gmail.com>
2020-05-02 13:13:51 -0400

    Declar Python 3.7 and 3.8 support
    AB#38

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ),
 )
